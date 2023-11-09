e3de062c51fd25888377239c3aa7479060153ac5
Mate Buljan <mate.buljan1@gmail.com>
2023-11-09 20:05:14 +0100

    update gitignore

--- WaveCompiler/autogen/WaveConfig.h
+++
@@ -1,3 +0,0 @@
-#define WAVE_COMPILER_PATH 		"C:/Users/mbuljan/Desktop/Projects/Wave/WaveCompiler/"
-#define WAVE_TESTS_PATH    		"C:/Users/mbuljan/Desktop/Projects/Wave/WaveTests/"
-#define WAVE_BINARY_PATH    	"C:/Users/mbuljan/Desktop/Projects/Wave/build/bin/"
