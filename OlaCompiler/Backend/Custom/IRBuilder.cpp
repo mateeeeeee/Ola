b9991b1e510f15dd198bc6769903d7fbb4fb32c7
Mate Buljan <mate.buljan1@gmail.com>
2024-01-22 02:17:03 +0100

    ir builder started

--- OlaCompiler/Backend/Custom/IRBuilder.cpp
+++ OlaCompiler/Backend/Custom/IRBuilder.cpp
@@ -1,8 +1,20 @@
 #include "IRBuilder.h"
+#include "IR.h"
 
 namespace ola
 {
 
+	IRBuilder::IRBuilder(IRContext& ctx) : ctx(ctx), insert_point(nullptr)
+	{
+
+	}
+
+	IRBuilder::IRBuilder(BasicBlock* insert_block) : ctx(insert_block->GetContext())
+	{
+		SetInsertPoint(insert_block);
+	}
+
+	IRBuilder::~IRBuilder() = default;
 
 }
 
--- OlaCompiler/Backend/Custom/IRBuilder.h
+++ OlaCompiler/Backend/Custom/IRBuilder.h
@@ -5,11 +5,27 @@ namespace ola
 {
 	class IRContext;
 	class BasicBlock;
+	class Instruction;
 
 	class IRBuilder
 	{
 	public:
+		explicit IRBuilder(IRContext& ctx);
+		explicit IRBuilder(BasicBlock* insert_block);
+		explicit IRBuilder(Instruction* insert_point);
+		OLA_NONCOPYABLE(IRBuilder)
+		~IRBuilder();
+
+		void SetInsertPoint(BasicBlock* _insert_block)
+		{
+			insert_block = _insert_block;
+			insert_point = &insert_block->back();
+		}
 
 	private:
+		IRContext& ctx;
+		BasicBlock*  insert_block;
+		Instruction* insert_point;
+	
 	};
 }
