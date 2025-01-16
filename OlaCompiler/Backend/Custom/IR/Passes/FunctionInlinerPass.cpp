6ea0a53e32ae1ef1e7f8409c019eef9bb1dffead
Mate Buljan <mate.buljan1@gmail.com>
2025-01-16 09:21:52 +0100

    inlining wip

---
+++ OlaCompiler/Backend/Custom/IR/Passes/FunctionInlinerPass.cpp
@@ -0,0 +1,135 @@
+#include <unordered_map>
+#include "FunctionInlinerPass.h"
+#include "Backend/Custom/IR/GlobalValue.h"
+#include "Backend/Custom/IR/IRBuilder.h"
+#include "Backend/Custom/IR/IRContext.h"
+
+namespace ola
+{
+
+	Bool FunctionInlinerPass::RunOn(Function& F, FunctionAnalysisManager& FAM)
+	{
+		Bool Changed = false;
+		std::vector<CallInst*> CallsToInline;
+		for (BasicBlock& BB : F)
+		{
+			for (Instruction& I : BB.Instructions())
+			{
+				if (CallInst* CI = dyn_cast<CallInst>(&I))
+				{
+					if (ShouldInline(CI)) CallsToInline.push_back(CI);
+				}
+			}
+		}
+		for (CallInst* CI : CallsToInline)
+		{
+			if (InlineFunction(CI))
+			{
+				Changed = true;
+			}
+		}
+		return Changed;
+	}
+
+	Bool FunctionInlinerPass::ShouldInline(CallInst* CI)
+	{
+		Function* Callee = CI->GetCalleeAsFunction(); 
+		if (!Callee || Callee->IsDeclaration())
+		{
+			return false;
+		}
+		if (CI->GetBasicBlock()->GetFunction() == Callee)
+		{
+			return false;
+		}
+		return Callee->Blocks().Size() <= 5;
+	}
+
+	Bool FunctionInlinerPass::InlineFunction(CallInst* CI)
+	{
+		IRContext& Ctx = CI->GetContext();
+		IRBuilder Builder(Ctx);
+		Function* Callee = CI->GetCalleeAsFunction();
+		BasicBlock* CallBlock = CI->GetBasicBlock();
+		Function* Caller = CI->GetCaller();
+
+		std::unordered_map<Value*, Value*> ValueMap;
+		auto ArgIt = Callee->ArgBegin();
+		for (unsigned i = 0; i < CI->GetNumOperands() - 1; ++i, ++ArgIt)
+		{
+			ValueMap[*ArgIt] = CI->GetOperand(i);
+		}
+
+		std::unordered_map<BasicBlock*, BasicBlock*> BBMap;
+		for (BasicBlock& BB : *Callee)
+		{
+			std::string name(BB.GetName());
+			name += ".inlined";
+			BasicBlock* InlinedBB = Builder.AddBlock(Caller, CallBlock->GetNextNode(), name);
+			BBMap[&BB] = InlinedBB;
+		}
+
+		for (auto& BBPair : BBMap)
+		{
+			BasicBlock* OrigBB = BBPair.first;
+			BasicBlock* NewBB = BBPair.second;
+
+			for (Instruction& I : OrigBB->Instructions())
+			{
+				if (isa<ReturnInst>(&I))
+				{
+					if (I.GetNumOperands() > 0)
+					{
+						Value* RetVal = I.GetOperand(0);
+						Value* MappedVal = ValueMap[RetVal];
+						CI->ReplaceAllUsesWith(MappedVal);
+					}
+					continue;
+				}
+
+				Instruction* NewInst = I.Clone();
+				NewBB->Instructions().PushBack(NewInst);
+				for (Uint32 i = 0; i < NewInst->GetNumOperands(); ++i)
+				{
+					Value* Op = NewInst->GetOperand(i);
+					if (Value* MappedOp = ValueMap[Op])
+					{
+						NewInst->SetOperand(i, MappedOp);
+					}
+				}
+				ValueMap[&I] = NewInst;
+			}
+		}
+
+		//BasicBlock* InlinedEntry = BBMap[&Callee->GetEntryBlock()];
+		//BasicBlock* CallBlockRemainder = CallBlock->SplitBasicBlock(Call);
+		//CallBlock->GetTerminator()->EraseFromParent();
+		//
+		////BranchInst::Create(InlinedEntry, CallBlock);
+		//
+		//for (Instruction& I : CallBlockRemainder->Instructions())
+		//{
+		//	if (PhiInst* Phi = dyn_cast<PhiInst>(&I))
+		//	{
+		//		for (Uint32 i = 0; i < Phi->GetNumIncomingValues(); ++i)
+		//		{
+		//			if (Phi->GetIncomingBlock(i) == CallBlock)
+		//			{
+		//				for (auto& BBPair : BBMap)
+		//				{
+		//					if (ReturnInst* RI = dyn_cast<ReturnInst>(BBPair.first->GetTerminator()))
+		//					{
+		//						Phi->AddIncoming(Phi->GetIncomingValue(i), BBPair.second);
+		//					}
+		//				}
+		//				//Phi->RemoveIncomingValue(i);
+		//				break;
+		//			}
+		//		}
+		//	}
+		//}
+		CI->EraseFromParent();
+		return true;
+	}
+
+}
---
+++ OlaCompiler/Backend/Custom/IR/Passes/FunctionInlinerPass.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Backend/Custom/IR/FunctionPass.h"
+#include "Backend/Custom/PassRegistry.h"
+
+namespace ola
+{
+	class Instruction;
+	class CallInst;
+
+	class FunctionInlinerPass : public FunctionPass
+	{
+		inline static Char id = 0;
+	public:
+		FunctionInlinerPass() : FunctionPass(id) {}
+		virtual Bool RunOn(Function& F, FunctionAnalysisManager& FAM) override;
+		static void const* ID() { return &id; }
+
+	private:
+		static Bool ShouldInline(CallInst* CI);
+		static Bool InlineFunction(CallInst* Call);
+	};
+	OLA_REGISTER_PASS(FunctionInlinerPass, "Loop Invariant Code Motion Pass Pass");
+
+	inline FunctionPass* CreateFunctionInlinerPass() { return new FunctionInlinerPass(); }
+}
