eacbd91c66eeeecf3a1b06a3453e0bec4b115d5c
Mate Buljan <mate.buljan1@gmail.com>
2025-10-27 22:49:31 +0000

    path concatenation fix on macos

--- OlaCompiler/Compiler/CompilerMacros.h
+++ OlaCompiler/Compiler/CompilerMacros.h
@@ -2,14 +2,22 @@
 #include "Core/Defines.h"
 #include "autogen/OlaConfig.h"
 
+// Platform-specific path concatenation
+// MSVC requires OLA_CONCAT (##), Clang/GCC can use direct string concatenation
+#if defined(_MSC_VER)
+	#define OLA_PATH_CONCAT(base, suffix) OLA_CONCAT(base, suffix)
+#else
+	#define OLA_PATH_CONCAT(base, suffix) base suffix
+#endif
+
 #if !LLVM_BACKEND
 
 #ifdef DEBUG
-#define OLA_EXE_PATH OLA_CONCAT(OLA_BINARY_PATH, "Debug/Ola.exe")
+#define OLA_EXE_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Debug/Ola.exe")
 #define OLA(...)		std::system(OLA_STRINGIFY(OLA_EXE_PATH --O0 --nollvm __VA_ARGS__))
 #define OLA_TEST(...)	std::system(OLA_STRINGIFY(OLA_EXE_PATH --O0 --nollvm --timeout --test __VA_ARGS__))
 #else
-#define OLA_EXE_PATH OLA_CONCAT(OLA_BINARY_PATH, "Release/Ola.exe")
+#define OLA_EXE_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Release/Ola.exe")
 #define OLA(...)		std::system(OLA_STRINGIFY(OLA_EXE_PATH --O3 --nollvm __VA_ARGS__))
 #define OLA_TEST(...)	std::system(OLA_STRINGIFY(OLA_EXE_PATH --O3 --nollvm --timeout --test __VA_ARGS__))
 #endif
@@ -17,11 +25,11 @@
 #else
 
 #ifdef DEBUG
-#define OLA_EXE_PATH OLA_CONCAT(OLA_BINARY_PATH, "Debug/Ola.exe")
+#define OLA_EXE_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Debug/Ola.exe")
 #define OLA(...)		std::system(OLA_STRINGIFY(OLA_EXE_PATH --O0 __VA_ARGS__))
 #define OLA_TEST(...)	std::system(OLA_STRINGIFY(OLA_EXE_PATH --O0 --timeout --test __VA_ARGS__))
 #else
-#define OLA_EXE_PATH OLA_CONCAT(OLA_BINARY_PATH, "Release/Ola.exe")
+#define OLA_EXE_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Release/Ola.exe")
 #define OLA(...)		std::system(OLA_STRINGIFY(OLA_EXE_PATH --O3 __VA_ARGS__))
 #define OLA_TEST(...)	std::system(OLA_STRINGIFY(OLA_EXE_PATH --O3 --timeout --test __VA_ARGS__))
 #endif
@@ -29,9 +37,9 @@
 #endif
 
 #if DEBUG
-#define OLA_STATIC_LIB_PATH OLA_CONCAT(OLA_BINARY_PATH, "Debug/olalib.lib")
+#define OLA_STATIC_LIB_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Debug/olalib.lib")
 #else
-#define OLA_STATIC_LIB_PATH OLA_CONCAT(OLA_BINARY_PATH, "Release/olalib.lib")
+#define OLA_STATIC_LIB_PATH OLA_PATH_CONCAT(OLA_BINARY_PATH, "Release/olalib.lib")
 #endif
 
 
