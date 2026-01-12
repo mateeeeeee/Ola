b0b962b3b7a4b18ca4426e176c5b89753fa75843
Mate Buljan <mate.buljan1@gmail.com>
2026-01-12 22:43:24 +0000

    update files

--- OlaPlayground/Test/test.s
+++ OlaPlayground/Test/test.s
@@ -11,11 +11,15 @@ _FP1:
 
 
 _FP2:
-.quad 4621256167635550208
+.quad 4617315517961601024
 
 
 _FP3:
 .quad 4619567317775286272
+
+
+_FP4:
+.quad 4621256167635550208
 
 
 .data
@@ -40,7 +44,7 @@ main:
 label0:
 push rbp
 mov rbp, rsp
-sub rsp, 112
+sub rsp, 128
 mov qword ptr [rbp - 8], r14
 movsd qword ptr [rbp - 16], xmm14
 movsd qword ptr [rbp - 24], xmm13
@@ -76,21 +80,54 @@ movsd xmm11, qword ptr [rbp - 40]
 mov rsp, rbp
 pop rbp
 ret
-label3:
-movsd qword ptr [rbp - 72], xmm13
-movsd xmm13, qword ptr [rbp - 72]
-comisd xmm13, qword ptr [rip + _FP2]
-sete r14b
-and r14b, 1
+label4:
+movsd qword ptr [rbp - 80], xmm12
+movsd xmm12, qword ptr [rbp - 80]
+comisd r14b, xmm12
 mov cl, r14b
 call Assert
-movsd xmm13, qword ptr [rip + _FP3]
-movsd qword ptr [rbp - 80], xmm13
-movsd xmm13, qword ptr [rbp - 80]
-comisd xmm13, qword ptr [rip + _FP3]
-sete r14b
-and r14b, 1
+lea r14, [rip + GlobalFloatC]
+movsd xmm12, qword ptr [r14]
+lea r14, [rip + GlobalFloatC]
+movsd xmm11, qword ptr [r14]
+comisd r14b, xmm12
+test r14b, r14b
+movsd xmm12, qword ptr [rip + _FP2]
+cmovne xmm12, xmm11
+movsd qword ptr [rbp - 88], xmm12
+movsd xmm12, qword ptr [rbp - 88]
+comisd r14b, xmm12
+mov cl, r14b
+call Assert
+movsd xmm15, qword ptr [rip + _FP3]
+movsd qword ptr [rbp - 96], xmm15
+movsd xmm12, qword ptr [rbp - 96]
+comisd r14b, xmm12
 mov cl, r14b
 call Assert
 mov qword ptr [rbp - 48], 0
 jmp label2
+label3:
+movsd qword ptr [rbp - 72], xmm13
+movsd xmm13, qword ptr [rbp - 72]
+comisd xmm13, qword ptr [rip + _FP4]
+sete r14b
+and r14b, 1
+mov cl, r14b
+call Assert
+lea r14, [rip + GlobalFloatC]
+movsd xmm13, qword ptr [r14]
+lea r14, [rip + GlobalFloatD]
+movsd xmm12, qword ptr [r14]
+lea r14, [rip + GlobalFloatC]
+movsd xmm11, qword ptr [r14]
+lea r14, [rip + GlobalFloatD]
+movsd xmm14, qword ptr [r14]
+comisd xmm13, xmm12
+seta r14b
+and r14b, 1
+test r14b, r14b
+movsd xmm12, xmm14
+je label4
+movsd xmm12, xmm11
+jmp label4
