477482e06c899fe04cc4118eea00f51c117296c7
Mate Buljan <mate.buljan1@gmail.com>
2026-01-12 00:31:46 +0000

    update infinite loop assemblz

--- OlaPlayground/Test/test.s
+++ OlaPlayground/Test/test.s
@@ -22,48 +22,58 @@ main:
 label0:
 push rbp
 mov rbp, rsp
-sub rsp, 112
+sub rsp, 160
 mov qword ptr [rbp - 8], r14
-movsd qword ptr [rbp - 16], xmm14
-movsd qword ptr [rbp - 24], xmm13
-movsd qword ptr [rbp - 32], xmm12
-movsd qword ptr [rbp - 40], xmm11
-movsd qword ptr [rbp - 48], xmm10
-movsd xmm14, xmm13
+mov qword ptr [rbp - 16], r13
+mov qword ptr [rbp - 24], r12
+movsd qword ptr [rbp - 32], xmm14
+movsd qword ptr [rbp - 40], xmm13
+movsd qword ptr [rbp - 48], xmm12
+movsd qword ptr [rbp - 56], xmm11
+movsd qword ptr [rbp - 64], xmm10
+movsd qword ptr [rbp - 72], xmm9
+movsd qword ptr [rbp - 80], xmm8
+movsd qword ptr [rbp - 88], xmm7
+label3:
+movsd xmm13, qword ptr [rip + _FP0]
+movsd qword ptr [rbp - 112], xmm13
+movsd xmm12, qword ptr [rip + _FP1]
+movsd qword ptr [rbp - 120], xmm12
+movsd xmm11, qword ptr [rbp - 112]
+movsd xmm14, qword ptr [rbp - 120]
+comisd xmm11, xmm14
+seta r14b
+and r14b, 1
+movsd xmm10, qword ptr [rbp - 112]
+movsd xmm9, qword ptr [rbp - 120]
+movsd xmm8, xmm9
 test r14b, r14b
 je label3
-movsd xmm14, xmm12
-label3:
-movsd xmm11, qword ptr [rip + _FP0]
-movsd qword ptr [rbp - 64], xmm11
-movsd xmm11, qword ptr [rip + _FP1]
-movsd qword ptr [rbp - 72], xmm11
-movsd xmm11, qword ptr [rbp - 64]
-movsd xmm10, qword ptr [rbp - 72]
-comisd xmm11, xmm10
-seta r14b
-and r14b, 1
-movsd xmm12, qword ptr [rbp - 64]
-movsd xmm13, qword ptr [rbp - 72]
-movsd qword ptr [rbp - 80], xmm14
-movsd xmm14, qword ptr [rbp - 80]
-comisd xmm14, qword ptr [rip + _FP0]
-seta r14b
-and r14b, 1
-mov cl, r14b
+movsd xmm8, xmm10
+movsd qword ptr [rbp - 128], xmm8
+movsd xmm7, qword ptr [rbp - 128]
+comisd xmm7, qword ptr [rip + _FP0]
+seta r13b
+and r13b, 1
+mov cl, r13b
 call Assert
-mov qword ptr [rbp - 56], 0
+mov qword ptr [rbp - 104], 0
 jmp label2
 label1:
 label2:
-mov r14, qword ptr [rbp - 56]
-mov rax, r14
+mov r12, qword ptr [rbp - 104]
+mov rax, r12
 mov r14, qword ptr [rbp - 8]
-movsd xmm14, qword ptr [rbp - 16]
-movsd xmm13, qword ptr [rbp - 24]
-movsd xmm12, qword ptr [rbp - 32]
-movsd xmm11, qword ptr [rbp - 40]
-movsd xmm10, qword ptr [rbp - 48]
+mov r13, qword ptr [rbp - 16]
+mov r12, qword ptr [rbp - 24]
+movsd xmm14, qword ptr [rbp - 32]
+movsd xmm13, qword ptr [rbp - 40]
+movsd xmm12, qword ptr [rbp - 48]
+movsd xmm11, qword ptr [rbp - 56]
+movsd xmm10, qword ptr [rbp - 64]
+movsd xmm9, qword ptr [rbp - 72]
+movsd xmm8, qword ptr [rbp - 80]
+movsd xmm7, qword ptr [rbp - 88]
 mov rsp, rbp
 pop rbp
 ret
