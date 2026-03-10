297c0c199ca4cf6bf345f5b1b42062c8eea6e767
Mate Buljan <mate.buljan1@gmail.com>
2026-03-10 22:44:09 +0000

    fix assert in llvm ir visitor

--- OlaCompiler/Backend/LLVM/LLVMIRVisitor.cpp
+++ OlaCompiler/Backend/LLVM/LLVMIRVisitor.cpp
@@ -1379,6 +1379,10 @@ namespace ola
 		else
 		{
 			Type const* array_expr_type = array_expr->GetType();
+			if (RefType const* ref_type = dyn_cast<RefType>(array_expr_type))
+			{
+				array_expr_type = ref_type->GetReferredType().GetTypePtr();
+			}
 			if (isa<PtrType>(array_expr_type))
 			{
 				PtrType const* ptr_type = cast<PtrType>(array_expr_type);
